d7e45a1d36bb8d262e178bac0aa71f797b1ed113
Maria Roodnitsky <maria.roodnitsky@gmail.com>
2022-05-02 11:21:21 -0700

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Maria Roodnitsky on 5/2/22.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
